dd07ec17e99c9cf97a3c5144d97b6cc7b17e48a2
Axel Kappel <axel.e.kappel@gmail.com>
2021-05-31 13:03:39 +0200

    linux compile fix

--- build.rs
+++ build.rs
@@ -1,13 +1,18 @@
+#[cfg(target_os = "windows")]
 use winres::WindowsResource;
 
+#[cfg(target_os = "windows")]
+fn compile_icon() {
+    let mut res = WindowsResource::new();
+    res.set_language(winapi::um::winnt::MAKELANGID(
+        winapi::um::winnt::LANG_ENGLISH,
+        winapi::um::winnt::SUBLANG_ENGLISH_US,
+    ));
+    res.set_icon("icon.ico");
+    res.compile().unwrap();
+}
+
 fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = WindowsResource::new();
-        res.set_language(winapi::um::winnt::MAKELANGID(
-            winapi::um::winnt::LANG_ENGLISH,
-            winapi::um::winnt::SUBLANG_ENGLISH_US,
-        ));
-        res.set_icon("icon.ico");
-        res.compile().unwrap();
-    }
+    #[cfg(target_os = "windows")]
+    compile_icon();
 }
