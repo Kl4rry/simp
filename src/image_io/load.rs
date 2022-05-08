dd28678d1c5f04d2d4e218e80c112a1b8528c56f
Axel Kappel <69117984+Kl4rry@users.noreply.github.com>
2022-05-08 11:56:55 +0200

    rm print

--- src/image_io/load.rs
+++ src/image_io/load.rs
@@ -83,10 +83,7 @@ pub fn load_raster(bytes: &[u8]) -> Option<Vec<Image>> {
             reader.no_limits();
             match reader.decode() {
                 Ok(image) => Some(vec![Image::new(image)]),
-                Err(err) => {
-                    println!("{:?}", err);
-                    None
-                }
+                Err(err) => None,
             }
         }
     }
